8e7996ca8f45fd1efb538fd3d96f737028aa570f
guillermogb88 <guillermo.garcia08@yahoo.com>
2021-06-10 17:00:34 -0700

    first part of the project done

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,21 @@
 class AnimalsController < ApplicationController
+    def index
+    animals = Animal.all 
+    render json: animals
+    end
+    def show 
+        animal = Animal.find(params[:id])
+        render json: animal 
+    end
+    def create 
+        animal = Animal.create(animal_params)
+         
+    end
+
+
+    private 
+    def animal_params
+        params.require(:animal).permit(:name, :latinname, :kingdom)
+    end
 end
+
